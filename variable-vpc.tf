bf89868c45cd2a912f88e5541eaa76854ff2ab6f
shashank1553 <116108268+shashank1553@users.noreply.github.com>
2024-11-27 16:08:46 +0530

    Update variable-vpc.tf

--- variable-vpc.tf
+++ variable-vpc.tf
@@ -18,11 +18,6 @@ variable "subnets" {
       name              = "dev-subnet-2",
       cidr              = "10.0.2.0/24",
       availability_zone = "us-east-1b"
-    },
-    "subnet-3" = {
-      name              = "dev-subnet-3",
-      cidr              = "10.0.4.0/24",
-      availability_zone = "us-east-1c"
     }
   }
 }
